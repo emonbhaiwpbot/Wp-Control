6e9741053872ba6cc2890eaff83a6144099075fe
emonbhaiwpbot <emonbhai.wp.bot@gmail.com>
2026-05-28 19:34:02 +0800

    Create gonfig.js

---
+++ gonfig.js
@@ -0,0 +1,15 @@
+module.exports = {
+botName:"€м𝐨Ⓝ",
+
+owner:"250701619925225",
+
+admins:[
+"8801309991724"
+],
+
+botNumber:"601166194923", // এখানে নিজের নাম্বার
+
+prefix:[".","#","!",""],
+
+image:"https://files.catbox.moe/sv6ge7.png"
+}
